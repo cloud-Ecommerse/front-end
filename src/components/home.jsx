6fb7c2d3f11516e02a333354d502dbdd468c03d3
CheaLivchea <6023010052@camtech.edu.kh>
2025-04-28 11:07:42 +0700

    uo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import Header from './header.jsx';
 import Hero from './hero.jsx';
 import Display from '../product-management/display.jsx';
-<<<<<<< HEAD
 import TopSelling from '../product-management/top-selling.jsx'; // Updated to camelCase naming
 import Footer from './footer.jsx';
 import ProductList from "../product-management/product.jsx"; // Included ProductList component
@@ -11,15 +10,6 @@ import ProductList from "../product-management/product.jsx"; // Included Product
 const App = () => { // Capitalized component name for consistency
   return (
     <BrowserRouter> {/* BrowserRouter wraps all components */}
-=======
-import TopSelling from '../product-management/top-selling.jsx'; // Changed to camelCase naming
-import Footer from './footer.jsx';
-import ProductList from "../product-management/product.jsx"; // Fixed path if needed
-
-const App = () => { // Capitalized component name for consistency
-  return (
-    <BrowserRouter> {/* Moved BrowserRouter to wrap all components */}
->>>>>>> d40cb0c33546f156fffa4fb05d79ada8c23e1594
       <Header />
       <ProductList />
       <Hero />
